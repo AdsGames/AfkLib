763177382420548a1df0fb4b662f4d30856d5fd0
Allan Legemaate <alegemaate@gmail.com>
2021-03-07 20:45:16 -0500

    Update build scripts

--- examples/ex_keyboard.cpp
+++ examples/ex_keyboard.cpp
@@ -3,6 +3,32 @@
 #include "../include/scene/Scene.h"
 #include "../include/services/Services.h"
 
+class Character : public afk::Sprite {
+ public:
+  Character(afk::Scene& scene, const float x, const float y)
+      : Sprite(scene, x, y) {
+    setTexture("lenna");
+    setSize(30, 30);
+  }
+
+  void update() {
+    afk::InputService& input = afk::Services::getInputService();
+
+    if (input.keyDown(afk::Keys::KEY_UP)) {
+      setPosition(x, y - 5);
+    }
+    if (input.keyDown(afk::Keys::KEY_DOWN)) {
+      setPosition(x, y + 5);
+    }
+    if (input.keyDown(afk::Keys::KEY_LEFT)) {
+      setPosition(x - 5, y);
+    }
+    if (input.keyDown(afk::Keys::KEY_RIGHT)) {
+      setPosition(x + 5, y);
+    }
+  }
+};
+
 class DemoScene : public afk::Scene {
  public:
   void start() {
@@ -18,46 +44,30 @@ class DemoScene : public afk::Scene {
     afk::AssetService& assets = afk::Services::getAssetService();
     assets.loadImage("lenna", "assets/lenna.png");
 
-    afk::Sprite& lenna = addObj<afk::Sprite>(*this, "lenna");
-    lenna.setSize(30, 30);
-    lenna.setPosition(100, 100);
-    lennaId = lenna.getId();
+    add<Character>(*this, 100, 100);
   }
 
-  void update() {
-    afk::InputService& input = afk::Services::getInputService();
-    afk::Sprite& lenna = get<afk::Sprite>(lennaId);
-
-    if (input.keyDown(afk::Keys::KEY_UP)) {
-      lenna.setPosition(lenna.getX(), lenna.getY() - 5);
-    }
-    if (input.keyDown(afk::Keys::KEY_DOWN)) {
-      lenna.setPosition(lenna.getX(), lenna.getY() + 5);
-    }
-    if (input.keyDown(afk::Keys::KEY_LEFT)) {
-      lenna.setPosition(lenna.getX() - 5, lenna.getY());
-    }
-    if (input.keyDown(afk::Keys::KEY_RIGHT)) {
-      lenna.setPosition(lenna.getX() + 5, lenna.getY());
-    }
-  }
+  void update() {}
 
   void stop() {
     afk::LoggingService& logger = afk::Services::getLoggingService();
     logger.log("Stopping!");
   }
+};
 
- private:
-  ObjId lennaId;
+class MainGame : public afk::Game {
+ public:
+  MainGame() : Game() {
+    afk::SceneService& scene = afk::Services::getSceneService();
+    scene.addScene<DemoScene>("demo");
+    scene.setNextScene("demo");
+
+    start();
+  }
 };
 
 int main(int argv, char** args) {
-  afk::Game game = afk::Game();
+  MainGame game = MainGame();
 
-  afk::SceneService& scenes = afk::Services::getSceneService();
-  scenes.addScene<DemoScene>("demo");
-  scenes.setNextScene("demo");
-
-  game.start();
   return 0;
 }
